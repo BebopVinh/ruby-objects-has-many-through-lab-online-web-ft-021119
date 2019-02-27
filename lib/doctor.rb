a599e5f2df365c52049dd54b0eb17a314b42e48c
Vinh Do <vinhdo07@gmail.com>
2019-02-27 03:31:42 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,4 +16,9 @@ class Doctor
   def new_appointment(date, patient)
     Appointment.new(date, patient, self)
   end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+  
 end #END OF CLASS
