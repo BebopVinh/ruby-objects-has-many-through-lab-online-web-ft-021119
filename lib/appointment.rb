78d40651c4ad9203f34929dba350bef48b5cc382
Vinh Do <vinhdo07@gmail.com>
2019-02-27 03:39:50 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,10 +3,11 @@ class Appointment
   @@all = []
   def initialize(date, patient, doctor)
     @date, @patient, @doctor = date, patient, doctor
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
 end
